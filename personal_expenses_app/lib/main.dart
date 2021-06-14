539376ada7875a16f860228152b2ee23f6854a02
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-06-14 13:21:13 +0530

    Progress

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:personal_expenses_app/transaction.dart';
 
 void main() => runApp(const MyApp());
@@ -22,10 +23,21 @@ class MyHomePage extends StatelessWidget {
     Transaction(
         id: 't1',
         title: 'New Shoes',
-        amount: 99.99,
+        amount: 2499.0,
+        date: DateTime.now()
+    ),
+    Transaction(
+        id: 't2',
+        title: 'CASIO Wrist Watch',
+        amount: 4599.0,
         date: DateTime.now()
     ),
   ];
+  // String? titleInput;
+  // String? amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +46,7 @@ class MyHomePage extends StatelessWidget {
         title: const Text("Flutter App"),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           const SizedBox(
             width: double.infinity,
@@ -41,6 +54,42 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
               child: Text("CHART!"),
               elevation: 5.0,
+            ),
+          ),
+          Card(
+            elevation: 5.0,
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      labelText: "Title",
+                    ),
+                    // onChanged: (String value) {
+                    //   titleInput = value;
+                    // },
+                  ),
+                  TextField(
+                    controller: amountController,
+                    decoration: const InputDecoration(
+                      labelText: "Amount",
+                    ),
+                    // onChanged: (value) {
+                    //   amountInput = value;
+                    // },
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        debugPrint(titleController.text);
+                        debugPrint(amountController.text);
+                      },
+                      child: const Text("Add Transaction", style: TextStyle(color: Colors.purple),),
+                  )
+                ],
+              ),
             ),
           ),
           Column(children: transaction.map((tx) {
@@ -56,7 +105,7 @@ class MyHomePage extends StatelessWidget {
                     ),
                     padding: const EdgeInsets.all(10.0),
                     child: Text(
-                      tx.amount!.toString(),
+                      "₹${tx.amount!}",
                       style: const TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20.0,
@@ -75,7 +124,7 @@ class MyHomePage extends StatelessWidget {
                     ),
                   ),
                   Text(
-                      tx.date!.toString(),
+                      DateFormat.yMMMd().format(tx.date!),
                     style: const TextStyle(
                       color: Colors.grey,
                     ),
