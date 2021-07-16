8937ba20c54ac1a260a695ae921a559ec41016c7
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-07-17 00:23:13 +0530

    [+] Bottom Navigation Added

--- meals_app/lib/screens/tabs_screen.dart
+++ meals_app/lib/screens/tabs_screen.dart
@@ -10,31 +10,37 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Widget>> _pages = const [
+    {'page': CategoriesScreen(), 'title': Text('Categories')},
+    {'page': FavoritesScreen(), 'title': Text('Your Favourites')},
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("Meals"),
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: "Categories",
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: "Favorites",
-                )
-              ],
-            ),
-          ),
-          body: const TabBarView(children: [
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],),
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title: _pages[_selectedPageIndex]['title'],
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(backgroundColor: Theme.of(context).primaryColor, icon: const Icon(Icons.category), label: "Categories"),
+            BottomNavigationBarItem(backgroundColor: Theme.of(context).primaryColor, icon: const Icon(Icons.star), label: "Favourites"),
+          ]
+      ),
     );
   }
 }
